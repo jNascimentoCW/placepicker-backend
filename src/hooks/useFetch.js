8089bbd09b3902a07d364d46c7307ffd81f07278
Jhonatan Nascimento <j.nascimento3138@gmail.com>
2024-02-16 14:16:38 -0300

    Finish project with backend

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,33 @@
+//functions starting with use is a function for react custom hooks
+import { useEffect, useState } from "react";
+
+export function useFetch(fetchFn, initialValue) {
+    const [isFetching, setIsFetching] = useState();
+    const [error, setError] = useState();
+    const [fetchedData, setFetchedData] = useState(initialValue);
+
+    useEffect(() => {
+        async function fetchData() {
+            setIsFetching(true);
+            try {
+                const data = await fetchFn();
+                setFetchedData(data);
+            } catch (error) {
+                setError({
+                    message: error.message || "Failed to fetch data.",
+                });
+            }
+
+            setIsFetching(false);
+        }
+
+        fetchData();
+    }, [fetchFn]);
+
+    return {
+        isFetching,
+        fetchedData,
+        setFetchedData,
+        error,
+    };
+}
